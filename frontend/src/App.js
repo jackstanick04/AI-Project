295593c987f2c614b80b49a0f6831d1fcd7e2ee6
Jack <stanickj@bc.edum>
2026-02-20 20:45:07 -0500

    text output

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,9 @@ function App() {
 
   // declaring a variable to store the file, declaring a function that is called to update the variable, and then setting the variable originally to null
   const [file, setFile] = useState(null);
+  // variables to check if we are loading (waiting for the gemini response) and store the actual gemini response
+  const [textDescription, setText] = useState("");
+  const [loading, setLoad] = useState(false);
 
   // this is the function that we run when a file is uploaded
   // lot of syntax, but the "evt" is the object pointing to what we uploaded, where it was in the html, etc. (everything related to the change)
@@ -22,6 +25,10 @@ function App() {
   // function to send the image to python; async makes the entire function wait until the await to refresh the page (because it takes some time to get the gemini response)
   const send = async() => {
 
+    // after submitting image, set to loading state and clear old text
+    setLoad(true);
+    setText("");
+
     // the image is a bunch of binary, but needs to be packaged as a form to be sent to flask
     const formData = new FormData();
     // just adds an image to be sent in the form data (form data can hold many files)
@@ -33,13 +40,14 @@ function App() {
       {
         method: 'POST',
         body: formData
-      });
+      })
 
-    // the data from flask contains a lot of info and is unorganized, so we convert to json, then only take the raw text
-    // again dont run code until this step happens (await)
-    const gemText = await fromFlask.json();
-    // output to console; description is the actual text we want in the json
-    console.log(gemText.description)
+    // json the data
+    const jsonData = await fromFlask.json();
+
+    // update for output; changes both at once for the same refresh
+    setText(jsonData.description);
+    setLoad(false);
 
   };
 
@@ -49,18 +57,34 @@ function App() {
     // remember slight html background, divs are just sections
     <div>
 
-      {/* just a title */}
-      <h1>Image Describer</h1>
-    
-      {/* this is the upload, we are taking in a file (only image types like png) and then when this happens we are calling the fileupload function above */}
-      <input 
-        type="file" 
-        onChange={fileUpload} 
-        accept="image/*" 
-      />
+      {/* upload section */}
+      <div>
+        {/* just a title */}
+        <h1>Image Describer</h1>
+      
+        {/* this is the upload, we are taking in a file (only image types like png) and then when this happens we are calling the fileupload function above */}
+        <input 
+          type="file" 
+          onChange={fileUpload} 
+          accept="image/*" 
+        />
 
-      {/* button to send the image to the python code using the send function */}
-      <button onClick = {send}>Submit</button>
+        {/* button to send the image to the python code using the send function */}
+        <button onClick = {send}>Submit</button>
+      
+      </div>
+
+      {/* output section */}
+      <div>
+
+        {/* short circuit ifs for the loading text or text output depending on the right state; empty text counts as false */}
+
+        {loading && <p>loading Gemini</p>}
+
+        {textDescription && <p>{textDescription}</p>}
+
+
+      </div>
 
     </div>
 
